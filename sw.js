ee59a0755d6253b8a11eb38091798c1481c965ee
noagrp <lihwlee11@gmail.com>
2026-09-12 03:16:57 +0800

    Cache dedicated favicon in Noan Clock v3

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE_NAME = 'noan-clock-v2';
+const CACHE_NAME = 'noan-clock-v3';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest?v=2',
-  './icon-192.png?v=2',
-  './icon-512.png?v=2',
-  './apple-touch-icon.png?v=2'
+  './manifest.webmanifest?v=3',
+  './favicon-32x32.png?v=3',
+  './icon-192.png?v=3',
+  './icon-512.png?v=3',
+  './apple-touch-icon.png?v=3'
 ];
 
 self.addEventListener('install', event => {
