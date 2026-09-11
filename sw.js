abaf8269ce5ee1e01024f706bb1c843712ca4756
noagrp <lihwlee11@gmail.com>
2026-09-12 01:59:05 +0800

    Add Noan Clock service worker

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'noan-clock-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest?v=1',
+  './icon-192.png?v=1',
+  './icon-512.png?v=1',
+  './apple-touch-icon.png?v=1'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+        return response;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+    )
+  );
+});
